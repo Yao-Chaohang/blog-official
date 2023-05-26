4d657b7c03d8e9ae887ad37f1cd36f17816f2f9b
Yao-Chaohang <chaohang.yao@foodsup.com>
2023-05-26 14:33:50 +0800

    添加pm2配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps: [
+      {
+        name: 'nuxt-app',
+        port: '85',
+        exec_mode: 'fork',
+        instances: '1',
+        script: './.output/server/index.mjs'
+      }
+    ]
+  }
